8924fce93a391f5da5a569561b998ee575b9decf
Mo2men-dev <momensameh5@gmail.com>
2024-09-12 06:13:51 +0300

    Refactoring: code to use Cell component and added some comments

--- Sudoko/src/App.tsx
+++ Sudoko/src/App.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react'
-import { border, solve } from './utils'
+import { solve } from './utils'
 import { useGlobalState } from './context/state'
+import Cell from './components/Cell'
 
 function App() {
+    // Get the global state
     const globalState = useGlobalState()
+
+    // TODO: Function to generate a random grids
     globalState.initalGrid = [
         [0,7,0,0,0,4,0,0,0],
         [0,3,0,0,0,0,2,0,1],
@@ -16,21 +20,29 @@ function App() {
         [0,0,0,3,7,0,0,0,0]
     ]
     
+    // Copy the initial grid to preserve the original state
     globalState.initalGridCopy = globalState.initalGrid.map(row => row.slice())
 
+    // The current grid being displayed
     const [grid, setGrid] = useState<number[][]>(globalState.initalGrid)
 
     useEffect(() => {
-
+        // Show answer without animation
         if (!globalState.animate) {
             setGrid(globalState.initalGridCopy)
             return
         }
 
+        // Play animation
         const interval = setInterval(() => {
             if (globalState.currAnimationIndx < globalState.steps.length) {
+                // The current cell being processed by the algorithm
                 globalState.currCell = globalState.steps[globalState.currAnimationIndx].cell
+
+                // Update the grid
                 setGrid(globalState.steps[globalState.currAnimationIndx].grid)
+
+                // Move to the next "Animation Frame"
                 globalState.currAnimationIndx++
             }
         }, 500)
@@ -47,11 +59,7 @@ function App() {
                             <div key={i} className='flex'>
                                 {
                                     row.map((cell, j) => {
-                                        return (
-                                            <div key={j} className={`px-4 py-2 select-none font-bold text-xl ${border(i, j)} ${(globalState.currCell.r === i && globalState.currCell.c === j + 1) && globalState.animate ? 'bg-blue-400 bg-opacity-25' : ''}`}>
-                                                {cell === 0 ? <>&nbsp;</> : <span className={`${globalState.initalGrid[i][j] !== 0 ? 'text-green-400' : 'text-yellow-400' }`}>{cell}</span>}
-                                            </div>
-                                        )
+                                        return <Cell key={j} cellVal={cell} i={i} j={j} />
                                     })
                                 }
                             </div>
@@ -60,10 +68,19 @@ function App() {
             }
             </div>
             <button onClick={() => {
+                // Start the animation
                 globalState.animate = true
+
+                // Clear the steps array
                 globalState.steps = []
+
+                // Reset the grid
                 setGrid(globalState.initalGrid)
+
+                // Solve the grid and store the steps (Trigger the useEffect)
                 solve(globalState.initalGridCopy, 0, 0, globalState.steps)
+
+                // Reset the current animation index
                 globalState.currAnimationIndx = 0
             }} className='mt-4 px-4 py-2 bg-blue-500 text-white font-bold rounded-md'>Solve</button>
         </div>
